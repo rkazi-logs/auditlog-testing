5f0e6aaa43687c3777899ff2a9177bc786aab296
ruhizd-code <ruhi.zd@gmail.com>
2026-06-15 13:17:57 -0400

    Remove buffer overflow example from error-test3.cpp
    Removed code that demonstrates a buffer overflow vulnerability.

--- error-test3.cpp
+++ error-test3.cpp
@@ -2,11 +2,5 @@
 #include <cstring>
 
 int main() {
-    // Buffer overflow vulnerability - CodeQL will flag this
-    char buffer[10];
-    const char* user_input = "This is a long string that will overflow the buffer";
-    strcpy(buffer, user_input);  // SECURITY: Buffer overflow
-    
-    std::cout << "Buffer: " << buffer << std::endl;
-    return 0;
+   
 }
