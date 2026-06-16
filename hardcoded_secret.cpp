8f9ca0d0606aedc1a97624597fcc20f8772ca0f3
ruhizd-code <ruhi.zd@gmail.com>
2026-06-16 17:02:13 -0400

    Introduce buffer overflow vulnerability in hardcoded_secret.cpp
    Added a buffer overflow vulnerability example in main function.

--- hardcoded_secret.cpp
+++ hardcoded_secret.cpp
@@ -11,3 +11,13 @@ int main() {
     
     return 0;
 }
+
+int main() {
+    // Buffer overflow vulnerability - CodeQL will flag this
+    char buffer[10];
+    const char* user_input = "This is a long string that will overflow the buffer";
+    strcpy(buffer, user_input);  // SECURITY: Buffer overflow
+    
+    std::cout << "Buffer: " << buffer << std::endl;
+    return 0;
+}
